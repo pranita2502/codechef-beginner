25bab443e057d98ad4e43b9e2f54a794cc3548bc
pranita2502 <pranitamandavgane1997@gmail.com>
2019-11-05 10:59:05 +0530

    Solution for valid Triangle

---
+++ 021. Valid Triangles/FLOW013.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * FLOW013
+ */
+public class FLOW013 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int sum = a + b + c;
+            if (sum == 180)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
