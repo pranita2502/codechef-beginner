f469e8b8c87f4df606665ec4ec67a6acd69f009b
pranita2502 <pranitamandavgane1997@gmail.com>
2019-11-05 09:53:08 +0530

    Solution For Chef annd Operators

---
+++ 019. Chef And Operators/CHOPRT.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * CHOPRT
+ */
+public class CHOPRT {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a > b)
+                System.out.println(">");
+            else if (a == b)
+                System.out.println("=");
+            else
+                System.out.println("<");
+        }
+        sc.close();
+    }
+}
